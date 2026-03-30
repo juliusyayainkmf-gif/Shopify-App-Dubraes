31285659c4886c2e29837b83cee7656170225ea3
Julius Yayain <julius.yayain.kmf@gmail.com>
2026-03-31 06:59:21 +0800

    test

--- app/routes/app.get-configs.jsx
+++ app/routes/app.get-configs.jsx
@@ -2,12 +2,17 @@ import { v2 as cloudinary } from "cloudinary";
 
 export const action = async () => {
   try {
-    cloudinary.config({
-      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-      api_key: process.env.CLOUDINARY_API_KEY,
-      api_secret: process.env.CLOUDINARY_API_SECRET,
-    });
+    // cloudinary.config({
+    //   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    //   api_key: process.env.CLOUDINARY_API_KEY,
+    //   api_secret: process.env.CLOUDINARY_API_SECRET,
+    // });
 
+    cloudinary.config({
+      cloud_name: "djhh5ylhc",
+      api_key: "151814683284192",
+      api_secret: "F7xR5W-WITmur6qLZYWXE6tsdho",
+    });
 
     const result = await cloudinary.api.resources({
       max_results: 50,
