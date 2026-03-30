2436ee1c3b4eec3e7b67fe2be722768d535dc49e
Julius Yayain <julius.yayain.kmf@gmail.com>
2026-03-31 06:45:32 +0800

    Update app.get-configs.jsx

--- app/routes/app.get-configs.jsx
+++ app/routes/app.get-configs.jsx
@@ -2,10 +2,16 @@ import { v2 as cloudinary } from "cloudinary";
 
 export const action = async () => {
   try {
+    // cloudinary.config({
+    //   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    //   api_key: process.env.CLOUDINARY_API_KEY,
+    //   api_secret: process.env.CLOUDINARY_API_SECRET,
+    // });
+
     cloudinary.config({
-      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-      api_key: process.env.CLOUDINARY_API_KEY,
-      api_secret: process.env.CLOUDINARY_API_SECRET,
+      cloud_name: "djhh5ylhc",
+      api_key: "151814683284192",
+      api_secret: "F7xR5W-WITmur6qLZYWXE6tsdho",
     });
 
     const result = await cloudinary.api.resources({
@@ -14,22 +20,27 @@ export const action = async () => {
 
     console.log("CLOUDINARY RAW:", result);
 
-    return new Response(JSON.stringify({
-      success: true,
-      count: result.resources.length,
-      resources: result.resources,
-    }), {
-      headers: { "Content-Type": "application/json" },
-    });
-
+    return new Response(
+      JSON.stringify({
+        success: true,
+        count: result.resources.length,
+        resources: result.resources,
+      }),
+      {
+        headers: { "Content-Type": "application/json" },
+      },
+    );
   } catch (err) {
     console.error("CLOUDINARY ERROR:", err);
 
-    return new Response(JSON.stringify({
-      success: false,
-      error: err.message,
-    }), {
-      headers: { "Content-Type": "application/json" },
-    });
+    return new Response(
+      JSON.stringify({
+        success: false,
+        error: err.message,
+      }),
+      {
+        headers: { "Content-Type": "application/json" },
+      },
+    );
   }
 };
